41abf0af3c2eb4049fe8a71d1bc8ba5adcf17e95
Tuomas <tuomas.palenius@gmail.com>
2014-02-26 06:44:35 -0500

    updated style

--- Global_aligner.cpp
+++ Global_aligner.cpp
@@ -1,6 +1,6 @@
 #include "Global_aligner.h"
 
-Global_aligner::Global_aligner(Sequence a, Sequence b){
+Global_aligner::Global_aligner(Sequence a, Sequence b) {
 	set_sequence1(a.toString());
 	set_sequence2(b.toString());
 	set_match_score(2);
@@ -9,7 +9,7 @@ Global_aligner::Global_aligner(Sequence a, Sequence b){
 	initialize_matrix(a.toString(), b.toString());
 }
 
-Global_aligner::Global_aligner(string a, string b){
+Global_aligner::Global_aligner(string a, string b) {
 	set_sequence1(a);
 	set_sequence2(b);
 	set_match_score(2);
@@ -18,82 +18,77 @@ Global_aligner::Global_aligner(string a, string b){
 	initialize_matrix(a, b);
 }
 
-vector<Sequence>* Global_aligner::traceback(){
-/*
-Aligns the sequence globally.
-
-ae: matrix in class Sequence_aligner is initialised and filled with
-    start values.
-le: result contains two sequences that are in alignment.
-aikakompleksisuus: walks through the matrix. Time complexity is O(N*M).
-
-*/
+/**
+  * Aligns the sequence globally.
+  * 
+  * assume: matrix in class Sequence_aligner is initialised and filled with
+  *  start values.
+  * result: contains two sequences that are in alignment.
+  * complexity: walks through the matrix. Time complexity is O(N*M).
+  */
+vector<Sequence>* Global_aligner::traceback() {
 	string result_sequence1;
 	string result_sequence2;
 
-	// Algoritmissa aloitetaan viimeisest‰ alkiosta --> kumpikin sekvenssi globaalisti rinnastettu.
+	// The algorithm starts from the last element --> both sequences globally aligned.
 	int pos = x_koko * y_koko - 1;
 
-	if(verbalize) cout << "traceback started, please wait. Pos: " << pos << endl;
+	if (verbalize) {cout << "traceback started, please wait. Pos: " << pos << endl;}
 
-	while(pos>0){
-		// jos match niin mene sinne
-		// hae x ja y-komponentti:
+	while (pos > 0) {
+		// if match, then go there.jos match niin mene sinne
+		// fetch the x and y-component:
 
 		/*
-			Polku katsotaan tbmp listasta.
-			Kun menn‰‰n ldiag --> lis‰‰ kummankin sekvenssin kirjain.
-			left --> kappi seq2, seq1 kirjain.
-			top --> kappi seq1, seq2 kirjain.
+			Path is checked from the traceback matrix -list.
+			When going left diagonal (ldiag), both sequence letter is added
+			left --> cap seq2, seq1 letter.
+			top --> cap seq1, seq2 letter.
 
-			Positionista tulee yh‰ pit‰‰ kirjaa.
+			Position must still be tracked.
 		*/
 		//cout << "Test, pos: " << pos << endl;
 
-		if(tbmp[pos][1] == 1){
+		if (tbmp[pos][1] == 1) {
 			result_sequence1 += get_seq1_letter(pos);
-			if(pos > x_koko){ // ei menn‰ ali seq2:sta
+			if (pos > x_koko) { // didn't go below seq2
 				result_sequence2 += get_seq2_letter(pos);
 			} else{
 				result_sequence2 += "*";
 			}
 			pos = pos - x_koko - 1;
-		} else if(tbmp[pos][0] == 1){ // horisontaali liike, lis‰t‰‰n seq2:een cap.
-			/*
-				tarkistetaan ettei menn‰ seq1:en indeksin ali:
-			*/
-			if(pos>0){
+		} else if (tbmp[pos][0] == 1) { // horizontal move, add cap into seq2
+
+			 //	check that won't go below the seq1:
+			if (pos > 0) {
 				result_sequence1 += get_seq1_letter(pos);
 				result_sequence2 += "*";
 				pos -= 1;
 			} else{
 				cout << "End1." << endl;
 			}
-		} else if(tbmp[pos][2] == 1){ // vertikaali liike, lis‰t‰‰n seq1:een cap.
-			/*
-				Ongelma: seq2:sta luetaan yli tai ali indeksin.
-				tarkistetaan ettei menn‰ taulukon rajojen ali:
-			*/
-			if(pos >= x_koko){ // ei menn‰ ali seq2:sen indeksin.
+		} else if (tbmp[pos][2] == 1){ // vertical move, add cap into seq1
+			// Check that seq2 is read over or below the index.
+			// Check that didn't go below the table bounds:
+			if (pos >= x_koko) { // didn't go below the seq2 index
 				result_sequence1 += "*";
 				result_sequence2 += get_seq2_letter(pos);
-			} else{
+			} else {
 				cout << "End2. Pos = " << pos << ", x_koko = " << x_koko << endl;
 			}
 			pos -= x_koko;
-		} else{
+		} else {
 			cout << "WARNING, NO TRACEBACK POINTER FOUND!: " << "mp[pos]: " << mp[pos] << ", pos = " << pos << endl;
 			print_matrix();
 			pos--;
 		}
 		//mp[pos] = 9;
-		//cout << "Printtaan kuljetun matriisin: " << endl;
+		//cout << "Printing the walked matrix: " << endl;
 		//print_matrix();
 	} // End While
 
 	// cout << "Errors: " << error_flag << endl;
 
-	// k‰‰nn‰ result ymp‰ri:
 	result_sequence1 = reverse(result_sequence1);
 	result_sequence2 = reverse(result_sequence2);
 
@@ -110,43 +105,43 @@ aikakompleksisuus: walks through the matrix. Time complexity is O(N*M).
 
 }
 
-/* Ylikuormitetaan koska halutaan erilainen alustus: */
+/* Override, because different initialization is wanted: */
 
-void Global_aligner::initialize_matrix(string seq1, string seq2){
-	if(verbalize) cout << "Alustan matriisin:" << endl;
-	y_koko = seq2.size()+1;
-	x_koko = seq1.size()+1;
+void Global_aligner::initialize_matrix(string seq1, string seq2) {
+	if (verbalize) {cout << "Alustan matriisin:" << endl;}
+	y_koko = seq2.size() + 1;
+	x_koko = seq1.size() + 1;
 
-	if(verbalize) cout << "\tx koko: " << x_koko << endl;
-	if(verbalize) cout << "\ty koko: " << y_koko << endl;
+	if (verbalize) {cout << "\tx koko: " << x_koko << endl;}
+	if (verbalize) {cout << "\ty koko: " << y_koko << endl;}
 
-	mp 		= new int[x_koko*y_koko];
-	if(verbalize) cout << "\tLuon traceback matriisin." << endl;
-	tbmp 	= new int*[x_koko*y_koko];
+	mp = new int[x_koko * y_koko];
+	if (verbalize) {cout << "\tLuon traceback matriisin." << endl;}
+	tbmp = new int*[x_koko * y_koko];
 
-    // Haluttu alustus on seuraavanlainen:
+    // The wanted initialization is as follows:
 	//   ------
 	//	| 0-1-2
 	//	|-1....
 	//	|-2....
-	//  T‰m‰ tarkoittaa ett‰ aukot tulevat kalliimmiksi.
+	//  This means that the caps become more expensive.
 	int add = 0;
-	for(int x=0;x<x_koko;x++){
+	for (int x = 0; x < x_koko; x++) {
 		mp[x] = 0 + add;
 		add--;
 	}
 	add = -1;
-	for(int y=1;y<y_koko;y++){
-		mp[x_koko*y] = 0 + add;
+	for (int y = 1; y < y_koko; y++) {
+		mp[x_koko * y] = 0 + add;
 		add--;
 	}
-	if(verbalize) cout << "\tAlustan traceback matriisin..." << endl;
-	for(int i=0; i<x_koko*y_koko; i++){
-		tbmp[i] = new int[2]; // [left,ldiag,top], mahdolliset arvot: 1, 0.
-		for(int j=0; j<3; j++){
-			tbmp[i][j] = 0;	// arvot alustetaan nolliksi.
+	if (verbalize) {cout << "\tAlustan traceback matriisin..." << endl;}
+	for (int i = 0; i < x_koko * y_koko; i++) {
+		tbmp[i] = new int[2]; // [left,ldiag,top], possible values: 1, 0.
+		for (int j = 0; j < 3; j++) {
+			tbmp[i][j] = 0;	// values are initialized as zero
 		}
 	}
-	if(verbalize) cout << "Matriisin alustus valmis." << endl;
+	if (verbalize) {cout << "Matrix initialized." << endl;}
 
 }
