ed2af8389bfdb3e8ae0d25a813d4715d5adf247c
Tuomas <tuomas.palenius@gmail.com>
2014-03-07 09:03:54 -0500

    cleaned code

--- seq_align_main.cpp
+++ seq_align_main.cpp
@@ -3,50 +3,46 @@
 #include <fstream>
 #include "Sequence_aligner.h"
 #include "Global_aligner.h"
-//#include "Local_aligner.h" // Ei käytetä.
+//#include "Local_aligner.h" // not used
 #include "Multi_aligner.h"
 #include "Sequence.h"
 
 using namespace std;
-/*
-
-	STAR-menetelmä sekvenssien monirinnastusta varten.
-
-*/
+/**
+ *	Center star method to multi-align sequences
+ */
 int main(int argc, char * argv[]) {
 
 	cout << "*** Sequence alignment ***" << endl;
 
-	if(argc>2){
+	if (argc > 2) {
 		string *sekv = new string[argc-1];
 
-		for(int i=1; i<argc; i++){
+		for (int i = 1; i < argc; i++) {
 			string tmp = argv[i];
-			sekv[i-1] = tmp;
+			sekv[i - 1] = tmp;
 		}
 
 		vector<Sequence> *se;
 
-		if(argc > 3){
+		if (argc > 3) {
 			cout << "\tUsing STAR-multialignment method.\n" << endl;
-			Multi_aligner ma(sekv, argc-1);
+			Multi_aligner ma(sekv, argc - 1);
 			cout << "Input sequences: " << endl;
 			ma.print();
 			se = ma.STAR();
-		} else{
+		} else {
 			cout << "\tUsing global alignment method.\n" << endl;
 			Global_aligner sa(sekv[0], sekv[1]);
 			sa.fill_matrix();
 			se = sa.traceback();
 		}
 
-		/*
-			Tulostetaan rinnastuksen tulokset:
-		*/
+		// Print the results of alignment:
 
 		cout << "\nResults:" << endl;
 
-		for(unsigned int i=0;i<se->size();i++){
+		for (unsigned int i = 0; i < se->size(); i++) {
 			cout << "\t" << se->at(i).toString() << endl;
 		}
 
@@ -56,10 +52,10 @@ int main(int argc, char * argv[]) {
 
 		delete []sekv;
 
-	} else if(argc==2){
+	} else if (argc == 2) {
 		cout << "Luen sekvenssit tiedostosta." << endl;
 		ifstream seqfile (argv[1]);
-		if(!seqfile.is_open()){
+		if (!seqfile.is_open()) {
 			cout << "Tiedoston avaaminen epaonnistui." << endl;
 			return 1;
 		}
@@ -67,39 +63,37 @@ int main(int argc, char * argv[]) {
 		vector<string> v;
 		string line;
 
-		while(getline(seqfile, line)){
+		while (getline(seqfile, line)) {
 			v.push_back(line); // Add the line to the end
 		}
 
 		vector<Sequence> *se;
 		string *sekv = new string[v.size()];
 
-		for(unsigned int i = 0; i < v.size(); i++){
+		for (unsigned int i = 0; i < v.size(); i++) {
     		cout << i+1 << ": " << v[i] << endl;
 			sekv[i] = v[i];
 		}
 
-		if(v.size() <= 1){
+		if (v.size() <= 1) {
 			cout << "\tFile doesn't have enough sequences. Exiting." << endl;
 			return 1;
-		} else if(v.size() > 2){
+		} else if (v.size() > 2) {
 			cout << "\tUsing STAR-multialignment method." << endl;
 			Multi_aligner ma(sekv, v.size());
 			se = ma.STAR();
-		} else{
+		} else {
 			cout << "\tUsing global alignment method.\n" << endl;
 			Global_aligner sa(sekv[0], sekv[1]);
 			sa.fill_matrix();
 			se = sa.traceback();
 		}
 
-		/*
-			Tulostetaan rinnastuksen tulokset:
-		*/
+        // print the results of alignment:
 
 		cout << "\nResults:" << endl;
 
-		for(unsigned int i=0;i<se->size();i++){
+		for (unsigned int i = 0; i <se->size(); i++) {
 			cout << "\t" << se->at(i).toString() << endl;
 		}
 
